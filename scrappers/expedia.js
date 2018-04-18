1dde608f7b19eca923562c07db1e12882d831bc4
Nacho Lopez <ncho.lopez@gmail.com>
2018-04-18 22:39:53 +0200

    Add expedia scrapper.

---
+++ scrappers/expedia.js
@@ -0,0 +1,82 @@
+const puppeteer = require('puppeteer');
+const browser = require('../browser').browser;
+const utils = require('../utils');
+
+const SELECTORS = {
+  TAB_HOTEL: '#tab-hotel-tab-hp',
+  INPUT_HOTEL: '#hotel-destination-hp-hotel',
+  SELECT_ADULTS: '#hotel-1-adults-hp-hotel',
+  INPUT_DATE_FROM: '#hotel-checkin-hp-hotel',
+  INPUT_DATE_TO: '#hotel-checkout-hp-hotel',
+  BUTTON_SEARCH: '#gcw-hotel-form-hp-hotel button.gcw-submit',
+  SUGGESTED_HOTEL: '#typeaheadDataPlain .results-item',
+  PRICE: 'article[data-automation="pinnedHotel"] .actualPrice',
+  NUMBER_REVIEWS:
+    'article[data-automation="pinnedHotel"] .hotelSearchResultReviewTotal'
+};
+
+const getData = async hotel => {
+  const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(0);
+  await page.setRequestInterception(true);
+  page.on('request', request => {
+    if (request.resourceType() === 'image') request.abort();
+    else request.continue();
+  });
+
+  await page.goto('https://www.expedia.es/');
+
+  let numReviews;
+  let precio;
+
+  try {
+    await page.click(SELECTORS.TAB_HOTEL);
+
+    const input = await page.$(SELECTORS.INPUT_HOTEL);
+    await input.click();
+    await input.type(utils.cleanPreposiciones(hotel));
+
+    await page.waitForSelector(SELECTORS.SUGGESTED_HOTEL, { timeout: 3000 });
+    await page.click(SELECTORS.SUGGESTED_HOTEL);
+
+    await page.evaluate(sel => {
+      return (document.querySelector(sel).value = '01/10/2018');
+    }, SELECTORS.INPUT_DATE_FROM);
+
+    await page.evaluate(sel => {
+      return (document.querySelector(sel).value = '02/10/2018');
+    }, SELECTORS.INPUT_DATE_TO);
+
+    await page.evaluate(sel => {
+      return (document.querySelector(sel).value = '1');
+    }, SELECTORS.SELECT_ADULTS);
+
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: 'networkidle0' }),
+      page.click(SELECTORS.BUTTON_SEARCH)
+    ]);
+
+    precio = await page.evaluate(
+      sel => parseInt(document.querySelector(sel).innerText, 10),
+      SELECTORS.PRICE
+    );
+
+    numReviews = await page.evaluate(
+      sel => document.querySelector(sel).innerText.match(/\d+/)[0],
+      SELECTORS.NUMBER_REVIEWS
+    );
+    await browser.closePage(page);
+  } catch (e) {
+    console.log(e);
+    await page.screenshot({ path: `./errors/${hotel}.png` });
+  }
+
+  return {
+    precio,
+    numReviews
+  };
+};
+
+module.exports = {
+  getData
+};
